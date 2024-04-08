24be0bd800e3c3aa70eec94eb62d77c6c213e35f
ppoziemski00 <161339787+ppoziemski00@users.noreply.github.com>
2024-04-08 15:20:31 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         
         System.out.printf("Hello world!");
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 15; i++) {
             System.out.println("i = " + i);
         }
     }
