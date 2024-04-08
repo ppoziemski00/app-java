4bd38072217a655f15a705dfa7e3d88e26641493
ppoziemski00 <161339787+ppoziemski00@users.noreply.github.com>
2024-04-08 15:30:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         String[] imiona = {"Piotr", "Anna", "Jan", "Leszek"}; 
         for (int i = 0; i < imiona.length; i++)
+            {
         System.out.println(i + 1 + ". " + imiona[i]);
+            }
         }
     }
